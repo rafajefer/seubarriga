b5a51f73c659336865afe40eae14b80f1ab4fab8
Rafael pena <rafael.pena@visie.com.br>
2020-10-23 22:13:12 -0300

    Configurando o eslint e criando o primeiro teste com jest

---
+++ test/jest.test.js
@@ -0,0 +1,10 @@
+test('Deve conhecer os principais assertivas do jest', () => {
+    let number = null;
+    expect(number).toBeNull();
+    number = 10;
+    expect(number).not.toBeNull(); // não pode ser null
+    expect(number).toBe(10); // é 10?
+    expect(number).toEqual(10); // é igual 10?
+    expect(number).toBeGreaterThan(10); // é maior que 10
+    expect(number).toBeLessThan(10); // é menor que 10
+});
