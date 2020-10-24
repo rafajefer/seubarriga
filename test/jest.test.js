fac438ee218006696946efdfe826ebfd9cd2338f
Rafael pena <rafael.pena@visie.com.br>
2020-10-23 23:40:24 -0300

    Testes com objetos

--- test/jest.test.js
+++ test/jest.test.js
@@ -1,4 +1,4 @@
-test('Deve conhecer os principais assertivas do jest', () => {
+test('Devo conhecer os principais assertivas do jest', () => {
     let number = null;
     expect(number).toBeNull();
     number = 10;
@@ -7,4 +7,15 @@ test('Deve conhecer os principais assertivas do jest', () => {
     expect(number).toEqual(10); // é igual 10?
     expect(number).toBeGreaterThan(9); // é menor que 10
     expect(number).toBeLessThan(11); // é maior que 10
+});
+
+test('Devo saber trabalhar com objetos', () => {
+    const obj = { name: 'John', mail: 'john@mail.com' };
+    expect(obj).toHaveProperty('name');
+    expect(obj).toHaveProperty('name', 'John');
+    expect(obj.name).toBe('John');
+
+    const obj2 = { name: 'John', mail: 'john@mail.com' };
+    expect(obj).toEqual(obj2);
+    expect(obj).toEqual(obj);
 });
